e7e64f37a1536d6e01cbc6cd605cf90394f22596
christianbriceno <crisping.br@gmail.com>
2024-05-30 16:41:33 -0400

    mostrando errores de validaciones en json

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
 
 class UpdateProductRequest extends FormRequest
 {
@@ -27,5 +30,20 @@ class UpdateProductRequest extends FormRequest
             'name'        => 'required|string|max:255',
             'price'       => 'required|numeric'
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(ValidationValidator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(response()->json($errors));
     }
 }
